29f316307c53383a9346ac98b112d827cda39b37
Suman Adhikari <expertxkid@gmail.com>
2024-02-24 07:38:50 -0600

    feat: complete 069-binding-to-custom-properties

---
+++ 005-components-and-databinding-deep-dive/069-binding-to-custom-properties/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements: { type: string, name: string, content: string }[] = [
+    {type: 'server', name: 'Testserver', content: 'Just a test!'},
+  ];
+}
